5bee1e9046765636c63929a04d7d67d940c83949
Jason Semken <jason.semken@gmail.com>
2023-03-11 09:58:58 +1000

    Extra details for future progress

--- WebAppTesting/BasicWebApp/wiki.go
+++ WebAppTesting/BasicWebApp/wiki.go
@@ -7,7 +7,7 @@
 //  Add a handler to make the web root redirect to /view/FrontPage.
 //  Spruce up the page templates by making them valid HTML and adding some CSS rules.
 //  Implement inter-page linking by converting instances of [PageName] to
-//  <a href="/view/PageName">PageName</a>. (hint: you could use regexp.ReplaceAllFunc to do this)
+//  <a href="/view/PageName">PageName</a>. (hint: you could use regexp.ReplaceAllFunc to do this
 
 package main
 

128adc60db968f2ad40c9d9ecbf4ec0b8f5d26a6
Jason Semken <jason.semken@gmail.com>
2023-05-08 21:31:04 +1000

    Complete newList function

--- ToDo List/main.go
+++ ToDo List/main.go
@@ -7,26 +7,35 @@ import (
 
 func main() {
 	//fmt.Println(loadList())
-	newList()
+	fmt.Println(newList())
 }
 
-// Reads directory for file named 'My_list.txt', if no file s found a new file is created
-func newList() {
+// Reads directory for file named 'My_list.txt', if file found data loaded to byte, if no file found empty file created
+func newList() []byte {
+	var data []byte
+
 	d, err := ioutil.ReadDir(".")
 	if err != nil {
 		fmt.Println("Error:")
 	}
-
-	// For loop to search found file in directory for file named 'My_list.txt'
 	for _, f := range d {
 		if f.Name() == "My_list.txt" {
 			fmt.Println("Yay! Found a file!")
+			data, _ := ioutil.ReadFile("My_list.txt")
+			return data
 		} else {
-			e := []byte("")
-			err := ioutil.WriteFile("My_list.txt", e, 0666)
-			if err != nil {
-				fmt.Println("Error: Doesn't wanna save a new file!!")
-			}
+			fmt.Println("No file Found :( Created new file!!")
+			newFile()
 		}
+	}
+	return data
+}
+
+// Creates new file
+func newFile() {
+	e := []byte("")
+	err := ioutil.WriteFile("My_list.txt", e, 0666)
+	if err != nil {
+		fmt.Println("Error: Doesn't wanna save a new file!!")
 	}
 }

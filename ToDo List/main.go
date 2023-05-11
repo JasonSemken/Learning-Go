6861a60549700c092f03d6851f4fcc058bc6916e
Jason Semken <jason.semken@gmail.com>
2023-05-11 16:34:08 +1000

    Added save to file fucntion

--- ToDo List/main.go
+++ ToDo List/main.go
@@ -7,12 +7,11 @@ import (
 	"strings"
 )
 
-var ListData []string
+var listData []string
 
 func main() {
-	//fmt.Println(loadList())
-	//fmt.Println(loadFile())
 	homePage()
+
 }
 
 // Home page for ToDo list
@@ -23,22 +22,58 @@ func homePage() {
 	fmt.Printf("2 - Save File          3 - Exit program\n\n")
 	fmt.Scan(&uI)
 	switch uI {
-	case "0":
+	case "0": // Create new file
 		fmt.Println("You selected 0")
 		newFile()
-	case "1":
+	case "1": // Load file
 		fmt.Println("Loading file")
-		printData(loadFile())
+		listData = loadFile()
 		editPage()
-	case "2":
+	case "2": // Save file
 		fmt.Println("You selected 2")
-	case "3":
+		saveFile(listData)
+		os.Exit(1)
+	case "3": // Exit program
 		fmt.Println("Closing program")
 		os.Exit(1)
 	default:
 		fmt.Println("Incorrect input")
 		homePage()
 	}
+}
+
+// Page for viewing and editing the TODO list
+func editPage() {
+	var t string
+	fmt.Printf("\n\nEditing page!!\n")
+	fmt.Println("0 - Add to list         1 - Remove from list")
+	fmt.Printf("2 - Print list          3 - Return to Home Page\n\n")
+	fmt.Scan(&t)
+	switch t {
+	case "0": // Add item to list
+		var i string
+		fmt.Println("What would you like to add?")
+		fmt.Scan(&i)
+		listData = addToList(i)
+		editPage()
+	case "1": // remove item from list - doesn't work
+		var r string
+		fmt.Println("You selected 1")
+		printData(listData)
+		fmt.Println("Enter key to remove list item")
+		fmt.Scan(&r)
+	case "2": // Print current list items
+		fmt.Println("You selected 2")
+		printData(listData)
+		editPage()
+	case "3": // Return to home page
+		fmt.Println("You selected 3")
+		homePage()
+	default:
+		fmt.Println("Incorrect input")
+		editPage()
+	}
+
 }
 
 // Reads directory for file named 'My_list.txt', if file found data loaded to byte, if no file found empty file created
@@ -71,6 +106,27 @@ func newFile() {
 	}
 }
 
+// Calls String to byte slice function and saves to file
+func saveFile(s []string) {
+	i := sStringToByte(s)
+	err := ioutil.WriteFile("My_list.txt", i, 0666)
+	if err != nil {
+		fmt.Println("Error: Doesn't wanna save a new file!!")
+	} else {
+		fmt.Print("Saved File!")
+	}
+}
+
+// converts string slice to byte slice
+func sStringToByte(l []string) []byte {
+	var s string
+	var k []byte
+	s = strings.Join(l, ",")
+	k = []byte(s)
+	return k
+}
+
+// Prints data in the TODO list, 1 item per line
 func printData(d []string) {
 	fmt.Printf("\n\nThis is what you have to do!!\n")
 	for i, s := range d {
@@ -78,26 +134,8 @@ func printData(d []string) {
 	}
 }
 
-func editPage() {
-	var t string
-	fmt.Printf("\n\nEditing page!!\n")
-	fmt.Println("0 - Add to list         1 - Remove from list")
-	fmt.Printf("2 - Print list          3 - Return to Home Page\n\n")
-	fmt.Scan(&t)
-	switch t {
-	case "0":
-		fmt.Println("You selected 0")
-	case "1":
-		fmt.Println("You selected 1")
-	case "2":
-		fmt.Println("You selected 2")
-		editPage()
-	case "3":
-		fmt.Println("You selected 3")
-		homePage()
-	default:
-		fmt.Println("Incorrect input")
-		editPage()
-	}
-
+// Adds user input to the listData variable then returns updated listData
+func addToList(line string) []string {
+	listData = append(listData, line)
+	return listData
 }
